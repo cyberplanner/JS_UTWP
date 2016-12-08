ea9c58d887b88e577bbf1e86509cb62422cd4e99
cyberplanner <sal@salweb.co.uk>
2016-12-08 01:38:38 +0000

    Conceptual Aside: Javascript and 'undefined'

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
-var a = "Hello World!";
+var a;
+a = "Hello world!";
+console.log(a);
 
-function  b() {
-  
+if (a === undefined) {
+  console.log('a is undefined!');
+  document.write('a is undefined!');
+} else {
+  console.log('a is defined!');
+  document.write('a is defined!');
+
 }
